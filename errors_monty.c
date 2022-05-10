27ea1a4049f285fd29fcc084185e855d66505023
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-05-10 16:33:29 -0700

    fix: betty style error, split error.c into two files

--- errors_monty.c
+++ errors_monty.c
@@ -57,34 +57,3 @@ int malloc_error(void)
 	fprintf(stderr, "Error: malloc failed\n");
 	return (EXIT_FAILURE);
 }
-
-/**
- * empty_stack_error - empty stack error
- * @lineNum: line number
- * Return: EXIT_FAILURE
- */
-int empty_stack_error(int lineNum)
-{
-        fprintf(stderr, "L%d: can't pint, stack empty\n", lineNum);
-        return (EXIT_FAILURE);
-}
-/**
- * short_stack_error - empty stack error
- * @lineNum: line number
- * Return: EXIT_FAILURE
- */
-int short_stack_error(int lineNum)
-{
-        fprintf(stderr, "L%d: can't swap, stack too short\n", lineNum);
-        return (EXIT_FAILURE);
-}
-/**
- * add_stack_error - empty stack error
- * @lineNum: line number
- * Return: EXIT_FAILURE
- */
-int add_stack_error(int lineNum)
-{
-        fprintf(stderr, "L%d: can't add, stack too short\n", lineNum);
-        return (EXIT_FAILURE);
-}
