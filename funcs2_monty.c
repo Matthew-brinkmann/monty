448799b35ce8500e944ec426471d15da948d059d
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-05-11 15:09:01 -0700

    fix: 0 number check on Mod and Div

--- funcs2_monty.c
+++ funcs2_monty.c
@@ -93,7 +93,7 @@ void monty_div(stack_t **stack, unsigned int line_number)
 		return;
 	}
 
-	if ((*stack)->next->next->n == 0)
+	if ((*stack)->next->n == 0)
 	{
 		division_error(line_number);
 		free_tokens();
